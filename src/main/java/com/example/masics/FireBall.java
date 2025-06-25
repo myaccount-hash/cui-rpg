2c03975f30b4ee14ffacfa09d8e496b743209049
myaccount-hash <shota.wake.private@gmail.com>
2025-06-25 11:37:26 +0900

    update

--- src/main/java/com/example/masics/FireBall.java
+++ src/main/java/com/example/masics/FireBall.java
@@ -1,23 +1,17 @@
 package com.example.masics;
 
-import com.example.commands.Command;
-import com.example.sessions.BattleSession;
 import com.example.sessions.Session;
 
-public class FireBall extends Command {
-    private Session selectSession;
-    private BattleSession battleSession;
+public class FireBall extends Masic {
     public FireBall(Session selectSession) {
         super("fireball", "ファイアボールを使う", "fireball", selectSession);
-        this.selectSession = selectSession;
-        this.battleSession = (BattleSession)selectSession.getParentSession();
     }
 
     @Override
     public boolean execute(String[] args) {
-        battleSession.getMonster().takeDamage(50);
+        monster.takeDamage(50);
         selectSession.stop();
-        battleSession.setLogText(battleSession.getMonster().getName() + "にファイアボールを使った！");
+        battleSession.setLogText(player.getName() + "はファイアボールを使った！");
         return true;
     }
 }
--- src/main/java/com/example/masics/Heal.java
+++ src/main/java/com/example/masics/Heal.java
@@ -1,22 +1,16 @@
 package com.example.masics;
 
-import com.example.commands.Command;
-import com.example.sessions.BattleSession;
 import com.example.sessions.Session;
 
-public class Heal extends Command {
-    private Session selectSession;
-    private BattleSession battleSession;
+public class Heal extends Masic {
     public Heal(Session selectSession) {
         super("heal", "回復魔法を使う", "heal", selectSession);
-        this.selectSession = (Session)selectSession;
-        this.battleSession = (BattleSession)selectSession.getParentSession();
     }
     @Override
     public boolean execute(String[] args) {
-        battleSession.getPlayer().heal(20);
+        player.heal(20);
         selectSession.stop();
-        battleSession.setLogText(battleSession.getPlayer().getName() + "は回復魔法を使った！");
+        battleSession.setLogText(player.getName() + "は回復魔法を使った！");
         return true;
     }
 }
