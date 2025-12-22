3d3cb6faea0e6cd1b366d569eff698ec06e3c417
Shota Wake <shotawake@ShotaMacBook-Air.local>
2025-12-22 14:59:03 +0900

    update

--- src/main/java/com/example/items/Armor.java
+++ src/main/java/com/example/items/Armor.java
@@ -1,10 +1,11 @@
 package com.example.items;
 
-import com.example.commands.Command;
-import com.example.entities.Entity;
 import java.util.List;
 
-// 防具の抽象クラス。一部の攻撃を強化する。Entity.javaのarmorフィールドに脱着可能。
+import com.example.commands.Command;
+import com.example.entities.Entity;
+
+// 防具の抽象クラス。Entity.javaのarmorフィールドに脱着可能。
 public abstract class Armor extends Item {
   protected int defense;
 
